aea3244af98c5a50fd27ef38b942b16e03a9a703
Mario Rubio <mario@mrrb.eu>
2023-02-26 12:35:37 +0100

    Add support for default variables

--- env.py
+++ env.py
@@ -1,36 +1,59 @@
 #!/usr/bin/python3
 
-import sys
+import argparse
 
 if __name__ == "__main__":
-  if len(sys.argv) <= 1:
-    exit
+  parser = argparse.ArgumentParser(
+      description='Gen environment file from yaml files.', add_help=True)
+  parser.add_argument('-s', '--set-values',
+                      action='store_true',
+                      dest='set_value',
+                      help='Set the default variable values for the environment file'
+                      )
+  parser.add_argument('-v', '--values-file',
+                      dest='values_file',
+                      help='JSON file with the default variable values',
+                      type=str
+                      )
+  parser.add_argument('files',
+                      nargs='+',
+                      help='YAML files',
+                      type=str
+                      )
 
-  files = sys.argv[1:]
+  args = parser.parse_args()
 
   matches = []
 
-  for file_name in files:
-    f = open(file_name)
+  for file_name in args.files:
+    with open(file_name) as f:
+      file_matches = []
+      for line in f:    
+        while True:
+          m = line.find('${')
 
-    file_matches = []
-    for line in f:    
-      while True:
-        m = line.find('${')
+          if m < 0:
+            break
+          else:
+            file_matches.append(line[m:])
+            line = line[m+1:]
 
-        if m < 0:
-          break
-        else:
-          file_matches.append(line[m:])
-          line = line[m+1:]
-
-    matches += [m.lstrip('${').split(':')[0].split('}')[0]
-                for m in file_matches]
-    f.close()
+      matches += [m.lstrip('${').split(':')[0].split('}')[0]
+                  for m in file_matches]
 
   matches = list(set(matches))
   matches.sort()
-  # print(set(matches))
+
+  env_data = {}
+  if args.set_value:
+    import json
+
+    values_file = args.values_file
+    if not values_file:
+      values_file = './env.json'
+
+    with open(values_file) as data:
+        env_data = json.load(data)
 
   for m in matches:
-    print(f"{m}=")
+    print(f"{m}={env_data.get(m) if env_data.get(m) else ''}")
