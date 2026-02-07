20fce7dc1ab479518a471e8cc51c9278ca9389c4
Mario Rubio <mario@mrrb.eu>
2026-02-07 23:37:23 +0100

    Add Vikunja service and env vars
    - Add Vikunja docker-compose definition and related files
    - Create Vikunja data and db ignore patterns

--- server.sh
+++ server.sh
@@ -169,7 +169,7 @@ function server_chyrp_lite_chk_fix () {
 
   # Create uploads directory if not exists
   _check_create_dir $_SERVICESPATH/chyrp-lite/uploads/
-  
+
   # Set correct permissions for uploads directory
   chown -R 33:33 $_SERVICESPATH/chyrp-lite/uploads/
   chmod -R 755 $_SERVICESPATH/chyrp-lite/uploads/
---
+++ services/vikunja/README.md
@@ -0,0 +1,5 @@
+# Vikunja
+
+[https://vikunja.io](https://vikunja.io)
+
+The to-do app to organize your life
