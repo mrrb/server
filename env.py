f4cd56538bd0d42864e71d3456407fe51ed852a5
Mario Rubio <mario@mrrb.eu>
2026-05-24 12:49:52 +0200

    Cleanup

--- env.py
+++ env.py
@@ -3,43 +3,41 @@
 import argparse
 
 if __name__ == "__main__":
+  # Parse arguments
   parser = argparse.ArgumentParser(
-      description='Gen environment file from yaml files.', add_help=True)
-  parser.add_argument('-s', '--set-values',
-                      action='store_true',
-                      dest='set_value',
-                      help='Set the default variable values for the environment file'
-                      )
-  parser.add_argument('-v', '--values-file',
-                      dest='values_file',
-                      help='JSON file with the default variable values',
-                      type=str
-                      )
-  parser.add_argument('-e', '--extra-values-file',
-                      dest='extra_values_file',
-                      help='JSON file with extra variable values',
-                      type=str
-                      )
-  parser.add_argument('-E', '--extra-env-pair',
-                      nargs='+',
-                      dest='extra_env_pair',
-                      help='Extra environment pair KEY=VALUE',
-                      type=str
-                      )
-  parser.add_argument('files',
-                      nargs='+',
-                      help='YAML files',
-                      type=str
-                      )
+    description='Gen environment file from yaml files.', add_help=True
+  )
+
+  parser.add_argument(
+    '-s', '--set-values', action='store_true', dest='set_value',
+    help='Set the default variable values for the environment file'
+  )
+  parser.add_argument(
+    '-v', '--values-file', dest='values_file', type=str,
+    help='JSON file with the default variable values'
+  )
+  parser.add_argument(
+    '-e', '--extra-values-file', dest='extra_values_file', type=str,
+    help='JSON file with extra variable values'
+  )
+  parser.add_argument(
+    '-E', '--extra-env-pair', nargs='+', dest='extra_env_pair', type=str,
+    help='Extra environment pair KEY=VALUE',
+  )
+  parser.add_argument(
+    'files', nargs='+', help='YAML files', type=str
+  )
 
   args = parser.parse_args()
 
+
+  # Find all variables in the files
   matches = []
 
   for file_name in args.files:
     with open(file_name) as f:
       file_matches = []
-      for line in f:    
+      for line in f:
         while True:
           m = line.find('${')
 
@@ -70,7 +68,7 @@ if __name__ == "__main__":
       if extra_values_file:
         with open(extra_values_file) as data:
           env_data = env_data | json.load(data)
-    except:
+    except Exception:
       pass
 
   extra_env_data = {}
@@ -79,7 +77,7 @@ if __name__ == "__main__":
       try:
         key, value = eep.split('=')
         extra_env_data[key] = value
-      except:
+      except Exception:
         pass
 
   env_data = {**extra_env_data, **env_data}
@@ -88,5 +86,7 @@ if __name__ == "__main__":
   matches = list(set(matches))
   matches.sort()
 
+
+  # Print the variables
   for m in matches:
     print(f"{m}={env_data.get(m) if env_data.get(m) else ''}")
